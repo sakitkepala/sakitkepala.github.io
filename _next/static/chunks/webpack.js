fbe88cffec86ee8263177cb034164cd50ad4e3b0
sakitkepala <andikapriyotama@gmail.com>
2021-05-09 01:00:14 +0700

    Test animasi scroll kartu notice

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1688210676f226015513";
+/******/ 	var hotCurrentHash = "4091b5c71699ba2365f5";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
