6f15944ba561a7de9ee61e68f9ca8079ecbf4492
sakitkepala <andikapriyotama@gmail.com>
2021-05-10 22:35:46 +0700

    IMP footer ditambah link ke repo source

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ed3f010abe3882dfe267";
+/******/ 	var hotCurrentHash = "888346879727942bfa27";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
